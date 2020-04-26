f172e1cc47c0981dc7016c023ab279224a91f02b
SuperNoice <SuperNoice@yandex.ru>
2020-04-26 21:26:11 +0300

    init

---
+++ individual/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace individual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Importbutton = new System.Windows.Forms.Button();
+            this.TextBoxIn = new System.Windows.Forms.RichTextBox();
+            this.TextBoxOut = new System.Windows.Forms.RichTextBox();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.TextBoxParamN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Processbutton = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Importbutton
+            // 
+            this.Importbutton.Location = new System.Drawing.Point(106, 32);
+            this.Importbutton.Name = "Importbutton";
+            this.Importbutton.Size = new System.Drawing.Size(137, 45);
+            this.Importbutton.TabIndex = 0;
+            this.Importbutton.Text = "Import";
+            this.Importbutton.UseVisualStyleBackColor = true;
+            this.Importbutton.Click += new System.EventHandler(this.Importbutton_Click);
+            // 
+            // TextBoxIn
+            // 
+            this.TextBoxIn.Location = new System.Drawing.Point(12, 109);
+            this.TextBoxIn.Name = "TextBoxIn";
+            this.TextBoxIn.Size = new System.Drawing.Size(353, 329);
+            this.TextBoxIn.TabIndex = 1;
+            this.TextBoxIn.Text = "";
+            // 
+            // TextBoxOut
+            // 
+            this.TextBoxOut.Location = new System.Drawing.Point(423, 109);
+            this.TextBoxOut.Name = "TextBoxOut";
+            this.TextBoxOut.Size = new System.Drawing.Size(365, 329);
+            this.TextBoxOut.TabIndex = 2;
+            this.TextBoxOut.Text = "";
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(549, 32);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(136, 45);
+            this.Savebutton.TabIndex = 3;
+            this.Savebutton.Text = "save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // TextBoxParamN
+            // 
+            this.TextBoxParamN.Location = new System.Drawing.Point(371, 60);
+            this.TextBoxParamN.Name = "TextBoxParamN";
+            this.TextBoxParamN.Size = new System.Drawing.Size(46, 20);
+            this.TextBoxParamN.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "N:";
+            // 
+            // Processbutton
+            // 
+            this.Processbutton.Location = new System.Drawing.Point(371, 243);
+            this.Processbutton.Name = "Processbutton";
+            this.Processbutton.Size = new System.Drawing.Size(46, 45);
+            this.Processbutton.TabIndex = 6;
+            this.Processbutton.Text = "-->";
+            this.Processbutton.UseVisualStyleBackColor = true;
+            this.Processbutton.Click += new System.EventHandler(this.Processbutton_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Location = new System.Drawing.Point(12, 84);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(52, 19);
+            this.Clearbutton.TabIndex = 7;
+            this.Clearbutton.Text = "clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.Processbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxParamN);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.TextBoxOut);
+            this.Controls.Add(this.TextBoxIn);
+            this.Controls.Add(this.Importbutton);
+            this.Name = "Form1";
+            this.Text = "Количество путей длинны N";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Importbutton;
+        private System.Windows.Forms.RichTextBox TextBoxIn;
+        private System.Windows.Forms.RichTextBox TextBoxOut;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.TextBox TextBoxParamN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Processbutton;
+        private System.Windows.Forms.Button Clearbutton;
+    }
+}
+
---
+++ individual/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace individual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Importbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.DefaultExt = ".txt";
+            openFile.AddExtension = true;
+            openFile.Title = "Открыть...";
+            openFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFile.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            TextBoxIn.Text = fileText;
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.DefaultExt=".txt";
+            saveFile.AddExtension = true;
+            saveFile.Title = "Сохранить как...";
+            saveFile.OverwritePrompt = true;
+            saveFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            if (saveFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFile.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, TextBoxOut.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void Processbutton_Click(object sender, EventArgs e)
+        {
+            TextBoxOut.Clear();
+
+            if (TextBoxParamN.Text.Length == 0)
+            {
+                TextBoxOut.AppendText("Параметр N не задан!");
+                return;
+            }
+            int N = System.Convert.ToInt32(TextBoxParamN.Text);
+
+            if (TextBoxIn.Text.Length == 0)
+            {
+                TextBoxOut.AppendText("Введите матрицу!");
+                return;
+            }
+            string reference = TextBoxIn.Text.Trim();
+            string[] str = reference.Split('\n');
+            int size = str.Length;
+            int[,] matr = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                string[] stl = str[i].Split(' ');
+
+                if (stl.Length != size)
+                {
+                    TextBoxOut.AppendText("Неверный формат матрицы!");
+                    return;
+                }
+
+                for (int j = 0; j < size; j++)
+                    matr[i, j] = System.Convert.ToInt32(stl[j]);
+            }
+
+            Graph G = new Graph();
+            G.setupGraph(matr);
+
+            int r = G.countWays(G.getVertexList()[0], N);
+            TextBoxOut.AppendText(System.Convert.ToString(r));
+
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            TextBoxIn.Clear();
+            TextBoxOut.Clear();
+        }
+    }
+
+    class Graph
+    {
+        private List<Vertex> vertexList;
+        private List<Edge> edgeList;
+        private string name;
+        private List<Edge> stack;
+
+        public Graph()
+        {
+            vertexList = new List<Vertex>();
+            edgeList = new List<Edge>();
+            stack = new List<Edge>();
+            name = "";
+        }
+
+        public Graph(string newName)
+        {
+            vertexList = new List<Vertex>();
+            edgeList = new List<Edge>();
+            stack = new List<Edge>();
+            name = newName;
+        }
+
+        public void setupGraph(int[,] matrSm)
+        {
+            int size = System.Convert.ToInt32(System.Math.Sqrt(matrSm.Length));
+            for (int i = 0; i < size; i++)
+                addVertex();
+
+            string type = "";
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrSm[i, j] * matrSm[j, i] > 0) type = "both";
+                    if (matrSm[i, j] > 0 && matrSm[j, i] < 0) type = "to";
+                    if (matrSm[i, j] < 0 && matrSm[j, i] > 0) type = "from";
+
+                    if (matrSm[i, j] != 0)
+                        addEdge(vertexList[i], vertexList[j], type);
+                }
+        }
+
+        public List<Vertex> getVertexList()
+        {
+            return vertexList;
+        }
+
+        public List<Edge> getEdgeList()
+        {
+            return edgeList;
+        }
+
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+
+        public Vertex addVertex()
+        {
+            Vertex newVertex = new Vertex(vertexList.Count());
+            vertexList.Add(newVertex);
+            return newVertex;
+        }
+
+        public Vertex addVertex(string newName)
+        {
+            Vertex newVertex = new Vertex(vertexList.Count(), newName);
+            vertexList.Add(newVertex);
+            return newVertex;
+        }
+
+        public Edge addEdge(Vertex transmitter, Vertex reciver, string Type)
+        {
+            Edge newEdge = new Edge(transmitter, reciver, edgeList.Count(), Type);
+            edgeList.Add(newEdge);
+
+            if (Type == "both")
+            {
+                if (!reciver.getAdjacentVertexList().Contains(transmitter))
+                    reciver.addAdjacentVertex(ref transmitter);
+                if (!transmitter.getAdjacentVertexList().Contains(reciver))
+                    transmitter.addAdjacentVertex(ref reciver);
+            }
+            if (Type == "to")
+            {
+                if (!transmitter.getAdjacentVertexList().Contains(reciver))
+                    transmitter.addAdjacentVertex(ref reciver);
+            }
+            if (Type == "from")
+            {
+                if (!reciver.getAdjacentVertexList().Contains(transmitter))
+                    reciver.addAdjacentVertex(ref transmitter);
+            }
+            return newEdge;
+        }
+
+        public Edge addEdge(string Type)
+        {
+            Vertex transmitter = new Vertex(vertexList.Count());
+            vertexList.Add(transmitter);
+            Vertex reciver = new Vertex(vertexList.Count());
+            vertexList.Add(reciver);
+            Edge newEdge = new Edge(transmitter, reciver, edgeList.Count(), Type);
+            edgeList.Add(newEdge);
+
+            if (Type == "both")
+            {
+                if (!reciver.getAdjacentVertexList().Contains(transmitter))
+                    reciver.addAdjacentVertex(ref transmitter);
+                if (!transmitter.getAdjacentVertexList().Contains(reciver))
+                    transmitter.addAdjacentVertex(ref reciver);
+            }
+            if (Type == "to")
+            {
+                if (!transmitter.getAdjacentVertexList().Contains(reciver))
+                    transmitter.addAdjacentVertex(ref reciver);
+            }
+            if (Type == "from")
+            {
+                if (!reciver.getAdjacentVertexList().Contains(transmitter))
+                    reciver.addAdjacentVertex(ref transmitter);
+            }
+            return newEdge;
+        }
+
+        private bool check(Vertex now, Vertex next)
+        {
+            bool f = false;
+            for (int i = 0; i < stack.Count; i++)
+                if ((stack[i].GetTransmitter().GetHashCode() == now.GetHashCode() && stack[i].GetReceiver().GetHashCode() == next.GetHashCode()) || (stack[i].GetTransmitter().GetHashCode() == next.GetHashCode() && stack[i].GetReceiver().GetHashCode() == now.GetHashCode()))
+                    f = true;
+
+            return f;
+        }
+        private int recCountWays(Vertex now, int wayleghtnow, int wayleghtneed)
+        {
+            int count = 0;
+            if (wayleghtnow == wayleghtneed) return 1;
+            List<Vertex> list = now.getAdjacentVertexList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Edge verse = new Edge(now, list[i], -1);
+                if (check(now, list[i])) continue;
+
+                stack.Add(verse);
+                count += recCountWays(list[i], wayleghtnow + 1, wayleghtneed);
+                stack.Remove(verse);
+            }
+
+            return count;
+        }
+
+        public int countWays(Vertex start, int wayLeght)
+        {
+            int count = 0;
+
+            count = recCountWays(start, 0, wayLeght);
+
+            return count;
+        }
+
+        public int countWays(int wayLeght)
+        {
+            int count = 0;
+
+            for (int i = 0; i < vertexList.Count; i++)
+                count += recCountWays(vertexList[i], 0, wayLeght);
+
+            return count;
+        }
+    }
+
+    class Edge
+    {
+        private Vertex TransmitterVertex, ReceiverVertex;
+        private string type, name;
+        private int id;
+
+        public Edge(Vertex transmitter, Vertex receiver, int newid)
+        {
+            this.TransmitterVertex = transmitter;
+            this.ReceiverVertex = receiver;
+            type = "none";
+            id = newid;
+        }
+
+        public Edge(Vertex transmitter, Vertex receiver, int newid, string newType)
+        {
+            this.TransmitterVertex = transmitter;
+            this.ReceiverVertex = receiver;
+            type = newType;
+            id = newid;
+        }
+
+        public void changeType(string newType)
+        {
+            type = newType;
+        }
+
+        public void SetTransmitter(Vertex newValue)
+        {
+            this.TransmitterVertex = newValue;
+        }
+
+        public void SetReceiver(Vertex newValue)
+        {
+            this.ReceiverVertex = newValue;
+        }
+
+        public Vertex GetTransmitter()
+        {
+            return this.TransmitterVertex;
+        }
+
+        public Vertex GetReceiver()
+        {
+            return this.ReceiverVertex;
+        }
+
+        public void EdgeSetup(Vertex value1, Vertex value2)
+        {
+            this.TransmitterVertex = value1;
+            this.ReceiverVertex = value2;
+        }
+    };
+
+    struct Vertex
+    {
+        private List<Vertex> adjacentVertexList;
+        private string name;
+        private int id;
+
+        public Vertex(int newid)
+        {
+            id = newid;
+            name = "";
+            adjacentVertexList = new List<Vertex>();
+        }
+
+        public Vertex(int newid, string newName)
+        {
+            id = newid;
+            name = newName;
+            adjacentVertexList = new List<Vertex>();
+        }
+
+        public void addAdjacentVertex(ref Vertex newVertex)
+        {
+            adjacentVertexList.Add(newVertex);
+        }
+
+        public List<Vertex> getAdjacentVertexList()
+        {
+            return adjacentVertexList;
+        }
+
+        public void setVertexName(string newName)
+        {
+            name = newName;
+        }
+
+    }
+
+}
